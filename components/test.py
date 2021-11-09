86ec216f287a72b685809e81670b3d494b7e53d5
tvanrys <tvanrys@fanshaweonline.ca>
2021-11-09 11:29:30 -0500

    dicts!

---
+++ components/test.py
@@ -0,0 +1,11 @@
+q1 = {
+    "question": "Do you live on earth?",
+    "yes" : 0,
+    "no" : 10
+}
+
+q2 = {
+    "question" : "Are you human?",
+    "yes" : 0,
+    "no" : 10
+}
---
+++ quiz.py
@@ -0,0 +1,19 @@
+q1 = {
+    "question": "Do you live on earth? yes / no: ",
+    "yes" : 0,
+    "no" : 10
+}
+
+q2 = {
+    "question" : "Are you human? yes / no: ",
+    "yes" : 0,
+    "no" : 10
+}
+
+count = 1
+
+ans1 = q1[input(q1["question"])]
+print(ans1)
+
+ans2 = q2[input(q2["question"])]
+print(ans2)
