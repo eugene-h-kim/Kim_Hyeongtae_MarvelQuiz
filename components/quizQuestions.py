08806c5e4bdb3ffbd03c3c928a1885c9823ff47a
Trevor - FanshaweC <tvanrys@fanshaweonline.ca>
2021-11-18 11:02:50 -0500

    quiz game starting point

--- components/quizQuestions.py
+++ components/quizQuestions.py
@@ -1,12 +1,12 @@
 questions = {
     "q1" : {
-        "question": "Do you live on earth?",
+        "question": "Do you live on earth? ",
         "yes" : 0,
         "no" : 10
     },
 
     "q2" : {
-        "question" : "Are you human?",
+        "question" : "Are you human? ",
         "yes" : 0,
         "no" : 10
     }
--- components/quizTally.py
+++ components/quizTally.py
@@ -2,16 +2,12 @@ from components import vars
 
 
 def total(value):
-    vars.answerCount += value
+    # do some logic to see which character you selected
 
-    # keep adding totals until you get the final tally
-    # then do some logic to see which character you selected
-
-    if vars.answerCount > 10:
+    if value <= 10:
         vars.character = vars.characters[0]
 
         print("It's " + vars.character)
-
         # add some emoji icons, or show the character image using the Pillow package
 
     
