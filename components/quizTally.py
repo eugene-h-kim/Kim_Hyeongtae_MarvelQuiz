fc23172166e2b815a52aa7c0062f6c03b33b2860
eugene-h-kim <h_kim123224@fanshaweonline.ca>
2021-12-09 00:25:33 -0500

    adding some changes to questions

--- components/quizTally.py
+++ components/quizTally.py
@@ -1,13 +1,41 @@
 from components import vars
+from PIL import Image
 
 
 def total(value):
     # do some logic to see which character you selected
 
-    if value <= 10:
+    if value <= 50:
         vars.character = vars.characters[0]
 
         print("It's " + vars.character)
         # add some emoji icons, or show the character image using the Pillow package
 
+        vars.character[0] = Image.open("iron_man.jpg")
+
+
+    if value <= 100:
+        vars.character = vars.characters[1]
+
+        print("It's " + vars.character)
+        # add some emoji icons, or show the character image using the Pillow package
+
+        vars.character[0] = Image.open("doctor_strange.jpg")
+        
     
+    if value <= 150:
+        vars.character = vars.characters[2]
+
+        print("It's " + vars.character)
+        # add some emoji icons, or show the character image using the Pillow package
+
+        vars.character[0] = Image.open("spider_man.jpg")
+        
+    
+    if value <= 200:
+        vars.character = vars.characters[3]
+
+        print("It's " + vars.character)
+        # add some emoji icons, or show the character image using the Pillow package
+
+        vars.character[0] = Image.open("thor.jpg")
--- quiz.py
+++ quiz.py
@@ -5,13 +5,58 @@ answer1 = questions["q1"][input(questions["q1"]["question"])]
 print(answer1)
 
 vars.quizTotal += answer1
-print("+++++++++++++++++++++++++++++++\n")
+print("*********************************************\n")
+
 
 answer2 = questions["q2"][input(questions["q2"]["question"])]
 print(answer2)
 
 vars.quizTotal += answer2
-print("+++++++++++++++++++++++++++++++\n")
+print("*********************************************\n")
+
+
+answer3 = questions["q3"][input(questions["q3"]["question"])]
+print(answer3)
+
+vars.quizTotal += answer3
+print("*********************************************\n")
+
+
+answer4 = questions["q4"][input(questions["q4"]["question"])]
+print(answer4)
+
+vars.quizTotal += answer4
+print("*********************************************\n")
+
+
+answer5 = questions["q5"][input(questions["q5"]["question"])]
+print(answer5)
+
+vars.quizTotal += answer5
+print("*********************************************\n")
+
+
+answer6 = questions["q6"][input(questions["q6"]["question"])]
+print(answer6)
+
+vars.quizTotal += answer6
+print("*********************************************\n")
+
+
+answer7 = questions["q7"][input(questions["q7"]["question"])]
+print(answer7)
+
+vars.quizTotal += answer7
+print("*********************************************\n")
+
+
+answer8 = questions["q8"][input(questions["q8"]["question"])]
+print(answer8)
+
+vars.quizTotal += answer8
+print("*********************************************\n")
+
+
 
 print("total so far: " + str(vars.quizTotal) + "\n")
 
