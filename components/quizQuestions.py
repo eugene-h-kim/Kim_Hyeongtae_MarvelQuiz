f16cc189a141e18780c81137450c47241df90868
eugene-h-kim <h_kim123224@fanshaweonline.ca>
2021-12-08 23:41:35 -0500

    adding some changes to questions

--- components/quizQuestions.py
+++ components/quizQuestions.py
@@ -1,48 +1,48 @@
 questions = {
     "q1" : {
-        "question": "Is this character an adult? ",
+        "question": "Is this character an adult? [yes/no]",
         "yes" : 0,
         "no" : 10
     },
 
     "q2" : {
-        "question" : "Did your character fight in the Civil War? ",
+        "question" : "Did your character fight in the Civil War? [yes/no]",
         "yes" : 0,
         "no" : 10
     },
     
     "q3" : {
-        "question" : "Does this character wear a mask? ",
+        "question" : "Does this character wear a mask? [yes/no]",
         "yes" : 0,
         "no" : 10
     },
     
     "q4" : {
-        "question" : "Does your character wear a red suit? ",
+        "question" : "Does your character wear a red suit? [yes/no]",
         "yes" : 0,
         "no" : 10
     },
     
     "q5" : {
-        "question" : "Does your character have a hammer? ",
+        "question" : "Does your character have a hammer? [yes/no]",
         "yes" : 140,
         "no" : 10
     },
     
     "q6" : {
-        "question" : "Does your character play the role of a doctor? ",
+        "question" : "Does your character play the role of a doctor? [yes/no]",
         "yes" : 40,
         "no" : 10
     },
     
     "q7" : {
-        "question" : "Does this character live with their aunt? ",
+        "question" : "Does this character live with their aunt? [yes/no]",
         "yes" : 110,
         "no" : 10
     },
     
     "q8" : {
-        "question" : "Did your character have only one child? ",
+        "question" : "Did your character have only one child? [yes/no]",
         "yes" : 20,
         "no" : 10
     }
